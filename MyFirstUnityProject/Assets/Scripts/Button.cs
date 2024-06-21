9e91902c323f91d3cc1560003ad04c7b670b1d02
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-06-22 02:42:52 +0530

    Create a Master scene - with 3 buttons (showing 1, 2, 3 as text on the button).

---
+++ MyFirstUnityProject/Assets/Scripts/Button.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string sceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
